006f996d4844652fde9926149058bf14357b466c
macie <macie@DESKTOP-GUK2RJ2>
2021-05-21 15:43:20 +0200

    install packages and change namespaces to ef core

--- TestNinja/Mocking/VideoService.cs
+++ TestNinja/Mocking/VideoService.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.IO;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 
 namespace TestNinja.Mocking
---
+++ TestNinja/Readme.txt
@@ -0,0 +1,5 @@
+﻿Install-Package Microsoft.EntityFrameworkCore.SqlServer
+Install-Package Microsoft.EntityFrameworkCore.Tools
+Install-Package Microsoft.EntityFrameworkCore.Design
+Install-Package NUnit
+Install-Package Moq
